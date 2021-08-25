893b37b06f437e07dc3422e1e7a5dc10b1d56dd2
Christopher <karger1986@gmail.com>
2021-08-25 20:22:05 +0200

    config

--- App.tsx
+++ App.tsx
@@ -2,15 +2,23 @@ import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import React, { useState } from "react";
 import { LogBox } from "react-native";
+import { config } from "./config";
 import Main from "./pages/main";
 
 LogBox.ignoreLogs(["Setting a timer"]);
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    "roboto-black": require("./assets/fonts/Roboto-Black.ttf"),
-    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
-  });
+const fetchFonts = async () => {
+  return Promise.all([
+    fetch(config.configUrl)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+      }),
+    Font.loadAsync({
+      "roboto-black": require("./assets/fonts/Roboto-Black.ttf"),
+      "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    }),
+  ]).then(() => {});
 };
 
 export default function App() {
---
+++ config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  configUrl: "https://crypto-wallet-98f66.web.app/config.json",
+};
