2e72de7b1e5a1c2c2f5d08e2918988ccbd25ca7c
Christopher <karger1986@gmail.com>
2021-10-14 20:44:37 +0200

    market chart

--- pages/market-data-item.tsx
+++ pages/market-data-item.tsx
@@ -1,12 +1,20 @@
 import * as shape from "d3-shape";
 import React, { useEffect, useState } from "react";
 import { Dimensions, Image, StyleSheet, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { LineChart } from "react-native-svg-charts";
 import GradientView from "~/components/gradient-view";
 import SafeArea from "~/components/safe-area";
 import SubPageHeader from "~/components/sub-page-header";
+import AppText from "~/components/text";
 import { Colors } from "~/constants";
-import { IMarketDataItem } from "~/models/market-data";
+import { IMarketDataItem, IMarketDataItemData } from "~/models/market-data";
+
+enum ChartView {
+  hours,
+  month,
+  week,
+}
 
 const MarketdataItem = (props) => {
   if (!props.route?.params?.item) {
@@ -15,18 +23,30 @@ const MarketdataItem = (props) => {
   const [name, setName] = useState("");
   const [chartData, setChartData] = useState<number[]>([]);
   const [trendColor, setTrendColor] = useState(Colors.green);
+  const [marketData, setMarketData] = useState<IMarketDataItemData>();
+  const [chartView, setChartView] = useState<ChartView>();
+
+  const changeView = (view: ChartView, data: IMarketDataItemData): void => {
+    const viewData =
+      view === ChartView.hours ? data.lastDayHistory : data.history;
+    let m = viewData.map((h) => h.price);
+
+    if (view === ChartView.week) {
+      m = m.slice(-7);
+    }
+
+    setChartData(m);
+    setChartView(view);
+    if (viewData[0].price > viewData[viewData.length - 1].price) {
+      setTrendColor(Colors.red);
+    }
+  };
 
   useEffect(() => {
     const { name, data } = props.route.params.item as IMarketDataItem;
-    const { lastDayHistory } = data;
-    const m = lastDayHistory.map((h) => h.price);
+    setMarketData(data);
     setName(name);
-    setChartData(m);
-    if (
-      lastDayHistory[0].price > lastDayHistory[lastDayHistory.length - 1].price
-    ) {
-      setTrendColor(Colors.red);
-    }
+    changeView(ChartView.week, data);
   }, []);
 
   return (
@@ -46,7 +66,51 @@ const MarketdataItem = (props) => {
             svg={{ stroke: trendColor }}
             curve={shape.curveNatural}
             contentInset={{ top: 10, bottom: 10, left: 0, right: 0 }}
+            animate={true}
           ></LineChart>
+        </View>
+        <View style={styles.chartButtonWraper}>
+          <TouchableOpacity
+            style={[
+              styles.chartButton,
+              chartView === ChartView.hours ? styles.activeChartButton : {},
+            ]}
+            onPress={() => {
+              if (marketData) {
+                changeView(ChartView.hours, marketData);
+              }
+            }}
+          >
+            <AppText>24 Std.</AppText>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[
+              styles.chartButton,
+              chartView === ChartView.week ? styles.activeChartButton : {},
+            ]}
+            onPress={() => {
+              if (marketData) {
+                changeView(ChartView.week, marketData);
+              }
+            }}
+          >
+            <AppText>7 Tage</AppText>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[
+              styles.chartButton,
+              chartView === ChartView.month ? styles.activeChartButton : {},
+            ]}
+            onPress={() => {
+              if (marketData) {
+                changeView(ChartView.month, marketData);
+              }
+            }}
+          >
+            <AppText>30 Tage</AppText>
+          </TouchableOpacity>
         </View>
       </SafeArea>
     </GradientView>
@@ -72,6 +136,24 @@ const styles = StyleSheet.create({
     left: 0,
     width: "100%",
     height: "100%",
+  },
+
+  chartButtonWraper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 20,
+  },
+
+  chartButton: {
+    marginHorizontal: 15,
+
+    padding: 15,
+  },
+
+  activeChartButton: {
+    borderBottomWidth: 1,
+    borderColor: Colors.white,
   },
 });
 
