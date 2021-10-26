e7f93208e9ca9973636acb9e50f995d7a6d90b21
Christopher <karger1986@gmail.com>
2021-10-26 16:40:33 +0200

    ref

--- services/format-number.ts
+++ services/format-number.ts
@@ -33,10 +33,15 @@ export const registerNumeralFormat = () => {
 
 export const formatNumber = (x: IFormatNumber): string => {
   const num = parseFloat(x.number.toString());
-
   if (num < 1) {
     return numeral(num).format("0,00.[0000]");
   }
 
-  return numeral(num).format("0,00.[00]");
+  const formatedNum = numeral(num).format("0,00.[00]");
+
+  // If number is for example 10,2 add ending 0 -> 10,20
+  if (formatedNum.split(",")[1]?.length === 1) {
+    return numeral(num).format("0,00.00");
+  }
+  return formatedNum;
 };
