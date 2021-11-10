7c5c84da9032bef35fc4d6a0635b3c3b90e62ee0
Christopher <karger1986@gmail.com>
2021-11-10 13:30:19 +0100

    fixed savea reav bug

--- components/safe-area.tsx
+++ components/safe-area.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView as DefaultSaveAreaView } from "react-native-safe-area-context";
+import { isIOS } from "~/constants";
 
 const SafeArea = (props) => {
+  if (isIOS) {
+    return (
+      <SafeAreaView style={[styles.areaView, props.style]}>
+        {props.children}
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <SafeAreaView style={[styles.areaView, props.style]}>
+    <DefaultSaveAreaView style={[styles.defaulArea, props.style]}>
       {props.children}
-    </SafeAreaView>
+    </DefaultSaveAreaView>
   );
 };
 const styles = StyleSheet.create({
   areaView: {
     flex: 1,
+  },
+  defaulArea: {
+    flex: 1,
+    paddingTop: 10,
   },
 });
 export default SafeArea;
--- constants.ts
+++ constants.ts
@@ -1,6 +1,7 @@
 import { Platform } from "react-native";
 
 export const isAndroid = Platform.OS === "android";
+export const isIOS = Platform.OS === "ios";
 
 export const UPDATE_WALLETS_EVENT = "event.updateWallets";
 
