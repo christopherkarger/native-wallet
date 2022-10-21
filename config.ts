d25bd3fe0fe27e5708cfddb015d6330b46fef055
Christopher <karger1986@gmail.com>
2022-10-21 11:22:11 +0200

    ref

--- config.ts
+++ config.ts
@@ -1,5 +1,3 @@
-import { API_KEY } from "./secrets";
-
 export const SupportedWallets = [
   {
     currency: "BTC",
@@ -89,7 +87,7 @@ export const SupportedCoins = [
     name: "Fantom",
   },
 ];
-
+const API_KEY = "A___OKb7vvS0uZtCCnefAiMSVMPKgE0P";
 export const SupportedUrls = {
   main: `https://api.blockchair.com/{{name}}/dashboards/address/{{address}}?key=${API_KEY}`,
   erc20: `https://api.blockchair.com/ethereum/erc-20/?{tokenAddress}/dashboards/address/{{address}}?key=${API_KEY}`,
--- services/firebase-init.ts
+++ services/firebase-init.ts
@@ -1,8 +1,16 @@
 import firebase from "firebase";
-import { FIREBASE_CONFIG } from "./firebase-config";
 
 export type firebaseDB = firebase.database.Reference;
 
-const fire = firebase.initializeApp(FIREBASE_CONFIG);
+const fire = firebase.initializeApp({
+  apiKey: "AIzaSyCpn1RAXi6VWQvGUNrk5dpl_KUANGpw-2M",
+  authDomain: "crypto-wallet-98f66.firebaseapp.com",
+  databaseURL:
+    "https://crypto-wallet-98f66-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "crypto-wallet-98f66",
+  storageBucket: "crypto-wallet-98f66.appspot.com",
+  messagingSenderId: "423385014326",
+  appId: "1:423385014326:web:511e78569e566945517897",
+});
 
 export default fire;
